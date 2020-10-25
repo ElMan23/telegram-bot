ff9f430843035f0874b9e223048a447dab8ba991
Elia Manara <e.manara90@gmail.com>
2020-10-25 08:48:15 +0100

    Code working!

---
+++ bot_main.py
@@ -0,0 +1,33 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import exchange
+
+TOKEN = "1392488110:AAF58cKrHgy7SiZjj28Av7Qr2q4w4G1FNFM"
+
+def extract_number(text):
+	return text.split()[1].strip()
+
+def convert_usd(update, context):
+	usd = float(extract_number(update.message.text))
+	eur = exchange.from_usd_to_eur(usd)
+	print(f'Eseguita conversione da {usd} USD a {eur} EUR')
+	update.message.reply_text(f'{eur} EUR')
+
+def convert_eur(update, context):
+	eur = float(extract_number(update.message.text))
+	usd = exchange.from_eur_to_usd(eur)
+	print(f'Eseguita conversione da {eur} EUR a {usd} USD')
+	update.message.reply_text(f'{usd} USD')
+
+def main():
+	upd = Updater(TOKEN, use_context=True)
+	disp = upd.dispatcher
+
+	disp.add_handler(CommandHandler("usd", convert_usd))
+	disp.add_handler(CommandHandler("eur", convert_eur))
+
+	upd.start_polling()
+
+	upd.idle()
+
+if __name__ == '__main__':
+	main()
